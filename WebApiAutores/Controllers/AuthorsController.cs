64010545f11b1c60048ce7050919e90da4daece7
Rafael Bolaños <animasdelmundo2@gmail.com>
2022-05-30 14:02:07 -0500

    Simple web Api finished, authors and books controllers done with migrations but pending to re-factor to include unit tests

--- WebApiAutores/Controllers/AuthorsController.cs
+++ WebApiAutores/Controllers/AuthorsController.cs
@@ -1,19 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiAutores.Entities;
 
 namespace WebApiAutores.Controllers
 {
+    //TODO: Pending to re-factor the code so unit-tests may apply
     [ApiController]
     [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Author>> Get()
+        private readonly ApplicationDbContext _context;
+
+        public AuthorsController(ApplicationDbContext context)
         {
-            return new List<Author>() { 
-                new Author() { Id = 1, Name = "Felipe" },
-                new Author() { Id = 2, Name = "Juan" } 
-            };
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Author>>> Get()
+        {
+            return await _context.Authors.Include(a => a.Books).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Author author)
+        {
+            _context.Add(author);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Edit(int id, Author author)
+        {
+            if(author.Id != id || id == 0 )
+                return BadRequest("Author not found");
+
+            _context.Update(author);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var deleteAuthor = await _context.Authors.AnyAsync(a => a.Id == id);
+            if(!deleteAuthor)
+                return NotFound();
+
+            _context.Remove(new Author() { Id = id });
+            await _context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
